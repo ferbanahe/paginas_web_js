7c7df5f4640d2f3933cdfa0d166f5cc943b9787c
ferbanahe <ferba6@yahoo.es>
2018-06-21 19:28:43 +0200

    js

--- 01_Datos/Hola_Funcion.js
+++ 01_Datos/Hola_Funcion.js
@@ -1,5 +1,5 @@
 /*JavaScript es de Oracle ->ECMAScript es su nombre oficial->ES6/ ES2015 es la actual versión
-/**crear funcion, saludar es el nombre, user es el parámetro. user cambiaría y var  se puede sustituir por const  si es un valor constante y pueden llamarse en mayuscula. El parámetro puede llevar una p u comenzar con mayúscula, pero no es obligatorio */
+/**crear funcion, saludar es el nombre, user es el parámetro. user cambiaría y var  se puede sustituir por const  si es un valor constante y pueden llamarse en mayuscula. El parámetro puede llevar una p y comenzar con mayúscula, pero no es obligatorio */
 function saludar(pUser) {
     const SALUDO = 'Hola'
     console.log(SALUDO, pUser)
