59ff4349f6266538383817cb163bdff419aa173b
ferbanahe <ferba6@yahoo.es>
2018-07-19 13:53:31 +0200

    Factura Web Final

---
+++ 06_Facturas_web_ES6/no_clases/factura.js
@@ -0,0 +1,21 @@
+function Factura (empresa, total) {
+    this.empresa = empresa
+    this.total = total
+}
+
+Factura.prototype.mostrar = function() {
+    console.log(this)
+}
+
+
+
+let f1 = new Factura("Boracay", 200)
+f1.concepto = 'Libros'
+let f2 = new Factura('Ghoogle', 3000)
+
+f1.mostrar()
+f2.mostrar()
+
+let a = {}
+let b = {x:23}
+console.log(a)
